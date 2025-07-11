212d3915a3fa367b28f7c42aaa7d7e63158834d3
Serhii Lisovyk <serlesovik@gmail.com>
2025-07-11 19:39:26 +0300

    Refactor tests

--- client/app/_utils/__tests__/queryString.test.js
+++ client/app/_utils/__tests__/queryString.test.js
@@ -1,10 +1,10 @@
-import { ALLOWED_SEARCH_PARAM_NAMES } from '@config/constants';
+import { ALLOWED_SEARCH_PARAM_NAMES } from '@config/constants'
 import {
   createSearchQueryString,
   createSearchValuesFromQueryString,
-} from '@utils/queryString';
+} from '@utils/queryString'
 
-describe('Tests for the createSearchQueryString function', () => {
+describe('createSearchQueryString utility function', () => {
   test('should return a valid query string with allowed params', () => {
     const queryParams = {
       text: 'life',
@@ -12,22 +12,23 @@ describe('Tests for the createSearchQueryString function', () => {
       category: 'inspiration',
       limit: 10,
       offset: 5,
-    };
-    const result = createSearchQueryString(queryParams);
+    }
+
+    const result = createSearchQueryString(queryParams)
     expect(result).toBe(
       'text=life&author=Albert+Einstein&category=inspiration&limit=10&offset=5'
-    );
-  });
+    )
+  })
 
   test('should filter out disallowed params', () => {
     const queryParams = {
       text: 'life',
       author: 'Albert Einstein',
       foo: 'bar', // This param is not allowed
-    };
-    const result = createSearchQueryString(queryParams);
-    expect(result).toBe('text=life&author=Albert+Einstein');
-  });
+    }
+    const result = createSearchQueryString(queryParams)
+    expect(result).toBe('text=life&author=Albert+Einstein')
+  })
 
   test('should filter out empty string, undefined, and null values', () => {
     const queryParams = {
@@ -35,34 +36,36 @@ describe('Tests for the createSearchQueryString function', () => {
       author: undefined,
       category: null,
       limit: 10,
-    };
-    const result = createSearchQueryString(queryParams);
-    expect(result).toBe('limit=10');
-  });
+    }
+
+    const result = createSearchQueryString(queryParams)
+    expect(result).toBe('limit=10')
+  })
 
   test('should return an empty string when no valid params are provided', () => {
     const queryParams = {
       foo: 'bar', // Disallowed param
       bar: 'baz', // Disallowed param
-    };
-    const result = createSearchQueryString(queryParams);
-    expect(result).toBe('');
-  });
+    }
+
+    const result = createSearchQueryString(queryParams)
+    expect(result).toBe('')
+  })
 
   test('should keep 0 as param value and not omit it', () => {
     const queryParams = {
       author: 'Alice',
       limit: 50,
       offset: 0,
-    };
-    const result = createSearchQueryString(queryParams);
-    expect(result).toBe('author=Alice&limit=50&offset=0');
-  });
-});
+    }
 
-describe('Tests for the createSearchValuesFromQueryString function', () => {
+    const result = createSearchQueryString(queryParams)
+    expect(result).toBe('author=Alice&limit=50&offset=0')
+  })
+})
+
+describe('createSearchValuesFromQueryString utility function', () => {
   test('should correctly create an object with allowed params from searchParams', () => {
-    // Mock searchParams object
     const mockSearchParams = {
       get: jest.fn((param) => {
         const params = {
@@ -71,12 +74,13 @@ describe('Tests for the createSearchValuesFromQueryString function', () => {
           category: 'category1',
           limit: '10',
           offset: '20',
-        };
-        return params[param] || null;
-      }),
-    };
+        }
 
-    const result = createSearchValuesFromQueryString(mockSearchParams);
+        return params[param] || null
+      }),
+    }
+
+    const result = createSearchValuesFromQueryString(mockSearchParams)
 
     expect(result).toEqual({
       text: 'some text',
@@ -84,31 +88,29 @@ describe('Tests for the createSearchValuesFromQueryString function', () => {
       category: 'category1',
       limit: '10',
       offset: '20',
-    });
+    })
 
-    // Check that the get method was called for each allowed param name
     ALLOWED_SEARCH_PARAM_NAMES.forEach((param) => {
-      expect(mockSearchParams.get).toHaveBeenCalledWith(param);
-    });
-  });
+      expect(mockSearchParams.get).toHaveBeenCalledWith(param)
+    })
+  })
 
   test('should return empty strings for missing searchParams', () => {
-    // Mock searchParams with only some params
     const mockSearchParams = {
       get: jest.fn((param) => {
         const params = {
           text: 'another text',
-        };
-        return params[param] || null;
+        }
+        return params[param] || null
       }),
-    };
+    }
 
-    const result = createSearchValuesFromQueryString(mockSearchParams);
+    const result = createSearchValuesFromQueryString(mockSearchParams)
 
     expect(result).toEqual({
       text: 'another text',
-    });
-  });
+    })
+  })
 
   test('should ignore unsupported params', () => {
     const mockSearchParams = {
@@ -118,25 +120,23 @@ describe('Tests for the createSearchValuesFromQueryString function', () => {
           author: 'Jane Doe',
           unsupportedParam1: 'value1', // Unsupported param
           unsupportedParam2: 'value2', // Unsupported param
-        };
-        return params[param] || null;
+        }
+        return params[param] || null
       }),
-    };
+    }
 
-    const result = createSearchValuesFromQueryString(mockSearchParams);
+    const result = createSearchValuesFromQueryString(mockSearchParams)
 
     expect(result).toEqual({
       text: 'valid text',
       author: 'Jane Doe',
-    });
+    })
 
-    // Ensure only allowed param names are queried
     ALLOWED_SEARCH_PARAM_NAMES.forEach((param) => {
-      expect(mockSearchParams.get).toHaveBeenCalledWith(param);
-    });
+      expect(mockSearchParams.get).toHaveBeenCalledWith(param)
+    })
 
-    // Ensure unsupported params are ignored
-    expect(mockSearchParams.get).not.toHaveBeenCalledWith('unsupportedParam1');
-    expect(mockSearchParams.get).not.toHaveBeenCalledWith('unsupportedParam2');
-  });
-});
+    expect(mockSearchParams.get).not.toHaveBeenCalledWith('unsupportedParam1')
+    expect(mockSearchParams.get).not.toHaveBeenCalledWith('unsupportedParam2')
+  })
+})
