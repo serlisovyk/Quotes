02a22395b1f6977719c8d81f1eb76522e74b2105
Serhii Lisovyk <serlesovik@gmail.com>
2025-11-22 21:42:07 +0200

    Add dialectModule into sequelize config

--- server/src/database/db.js
+++ server/src/database/db.js
@@ -1,8 +1,10 @@
+import pg from 'pg'
 import { Sequelize } from 'sequelize'
 import { DATABASE_URL, DB_DIALECT } from '../config/config.js'
 
 const sequelize = new Sequelize(DATABASE_URL, {
   dialect: DB_DIALECT,
+  dialectModule: pg,
   dialectOptions: {
     ssl: {
       require: true,
